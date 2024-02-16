fe387eb37d83c0bf0585fb6ad579eb8639772ebe
Aleah White <aleahwhite55@gmail.com>
2024-02-16 13:38:22 -0600

    defined model columns, and defined the models relationships in index

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,18 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // id has int datatype, no allowing null, primary key, and autoincrement
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+    // the name has a string datatype, and doesnt allow null
   },
   {
     sequelize,
