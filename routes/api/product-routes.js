fc78ad298a23a64c2b6f6e898f3226e2a24d2943
Aleah White <aleahwhite55@gmail.com>
2024-02-16 22:41:09 -0600

    added routes for products,categories, and tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models/index-model
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+      model: Category,
+      attributes: ['category_name']
+    }]
+  })
+  .then(ProductData => res.json(ProductData))
+  .catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['product_name', 'price', 'stock'],
+    include: [{
+      model: Category,
+      attributes: ['category_name']
+    }]
+  })
+  .then(ProductData => {
+    if (!ProductData) {
+      res.status(404).json({ message: 'No product found with this id!'});
+      return;
+    }
+    res.json(ProductData);
+  })
+  .catch (error => {
+    console.log(error);
+    res.status(500).json(error);
+  });
 });
 
 // create new product
@@ -57,10 +90,9 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       if (req.body.tagIds && req.body.tagIds.length) {
-        
-        ProductTag.findAll({
-          where: { product_id: req.params.id }
-        }).then((productTags) => {
+        ProductTag.findAll({ where: { product_id: req.params.id }
+        })
+        .then((productTags) => {
           // create filtered list of new tag_ids
           const productTagIds = productTags.map(({ tag_id }) => tag_id);
           const newProductTags = req.body.tagIds
@@ -71,8 +103,7 @@ router.put('/:id', (req, res) => {
               tag_id,
             };
           });
-
-            // figure out which ones to remove
+          // figure out which ones to remove
           const productTagsToRemove = productTags
           .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
           .map(({ id }) => id);
@@ -94,6 +125,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(ProductData => {
+    if (!ProductData){
+      res.status(404).json({ message: 'No product found with this id!'});
+      return;
+    }
+    res.json(ProductData);
+  })
+  .catch (error => {
+    console.log(error);
+    res.status(500).json(error);
+  });
 });
 
 module.exports = router;
